376ec127bc50b71d3a3f39ba239885442d0443fc
Milutin Spaic <milutin.spaic@bankom.rs>
2020-10-07 20:04:31 +0200

    Add project files.

---
+++ HotelReservations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HotelReservations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
