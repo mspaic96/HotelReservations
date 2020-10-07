b86a32012f95d756515aeeaef1f8d155d24efa17
Milutin Spaic <milutin.spaic@bankom.rs>
2020-10-07 23:34:23 +0200

    Ubacena klasa Hotel!

---
+++ HotelReservations/Hotel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservations
+{
+    class Hotel
+    {
+        private const int Days = 365;
+        private int[,] _array;
+
+        public Hotel(int size)
+        {
+            if(size > 1000 ) throw new ArgumentException("The size of hotel should be smaller than 1000!");
+            _array = new int[size,Days];
+        }
+        public  bool MakeReservation(int startDay, int endDay)
+        {
+            if (startDay < 0 || endDay >= 365) return false;
+            var checkAvailabity = false;
+            for (var i = 0; i < _array.GetLength(0); i++)
+            {
+                if (checkAvailabity) break;
+                for (var j = startDay; j <= endDay; j++)
+                {
+                    if (_array[i, j] == 1) break;
+                    if (j == endDay)
+                    {
+                        checkAvailabity = true;
+                        ConfirmReservation(i, startDay, endDay);
+                    }
+                }
+
+            }
+
+            return checkAvailabity;
+        }
+
+        public  void ConfirmReservation(int room, int startDay, int endDay)
+        {
+            for (var i = startDay; i <= endDay; i++)
+            {
+                _array[room, i] = 1;
+            }
+        }
+    }
+}
--- HotelReservations/Program.cs
+++ HotelReservations/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 
+
 namespace HotelReservations
 {
-    class Program
+   class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the number of rooms in hotel(size of your hotel)!");
+
+            var size = Convert.ToInt32(Console.ReadLine());
+
+            var hotel = new Hotel(size);
+
+            Console.WriteLine("Enter the startDay! If you want to exit press Enter!");
+            var start = Console.ReadLine();
+            while (start != "")
+            {
+                var startDay = Convert.ToInt32(start);
+                Console.WriteLine("Enter the endDay!");
+                var endDay = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(hotel.MakeReservation(startDay, endDay)
+                    ? "Reservation accepted!"
+                    : "Reservation declined!");
+
+                Console.WriteLine("Enter the startDay! If you want to exit press Enter!");
+                start = Console.ReadLine();
+            }
         }
+
+       
     }
 }
